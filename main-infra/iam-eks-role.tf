cae20424b723413fb22201f0c1d24d41e9f31054
shlomo-b <shlomobarzili@gmail.com>
2025-11-04 21:32:49 +0200

    update iam role

--- main-infra/iam-eks-role.tf
+++ main-infra/iam-eks-role.tf
@@ -22,20 +22,18 @@ module "iam_eks_role" {
 
 # # role for aws load balancer controller ingress
 
+
 module "iam_eks_role_alb_controller" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.39.1"
 
   role_name = "aws-load-balancer-controller-role"
-
   attach_load_balancer_controller_policy = true
 
   oidc_providers = {
     one = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"] # ns:name
+      namespace_service_accounts = ["aws-alb-controller:aws-load-balancer-controller"]
     }
   }
-
-  depends_on = [module.eks]
 }
