b01671071084f60152c7e9c701ba674f105c0d01
shlomo-b <shlomobarzili@gmail.com>
2025-11-09 02:00:47 +0200

    add new iam role

--- main-infra/iam-eks-role.tf
+++ main-infra/iam-eks-role.tf
@@ -22,29 +22,28 @@ module "iam_eks_role" {
 
 # # role for aws load balancer controller ingress
 
-
-data "aws_iam_policy_document" "alb_extra" {
-  statement {
-    sid     = "AllowDescribeListenerAttributes"
-    effect  = "Allow"
-    actions = [
-      "elasticloadbalancing:DescribeListenerAttributes"
-    ]
-    resources = ["*"]
-  }
-}
-
+# Small managed policy: grants permission to DescribeListenerAttributes for ALB
 resource "aws_iam_policy" "alb_extra" {
   name   = "AWSLoadBalancerControllerExtra"
-  policy = data.aws_iam_policy_document.alb_extra.json
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Sid      = "AllowDescribeListenerAttributes",
+      Effect   = "Allow",
+      Action   = ["elasticloadbalancing:DescribeListenerAttributes"],
+      Resource = ["*"]
+    }]
+  })
 }
 
+# IAM Role for the AWS Load Balancer Controller with built-in and extra policies
 module "iam_eks_role_alb_controller" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.28.0"
 
   role_name                              = "aws-load-balancer-controller-role"
   attach_load_balancer_controller_policy = true
+  attach_load_balancer_controller_targetgroup_binding_only_policy = true
 
   role_policy_arns = {
     extra = aws_iam_policy.alb_extra.arn
@@ -57,7 +56,24 @@ module "iam_eks_role_alb_controller" {
     }
   }
 
-  depends_on = [
-    module.eks
-  ]
+  depends_on = [module.eks]
+}
+
+
+# IAM Role for external-dns
+module "iam_eks_role_external-dns" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.28.0"
+
+  role_name                              = "external-dns-role"
+  attach_external_dns_policy             = true
+  
+  oidc_providers = {
+    one = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["external-dns:external-dns"]
+    }
+  }
+
+  depends_on = [module.eks]
 }
